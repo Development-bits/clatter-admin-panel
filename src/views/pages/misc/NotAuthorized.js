e0b9853af294ae9ad6d4ef1bed3cc6631b4a2d6b
naveed <development.bits@gmail.com>
2023-08-16 17:20:58 +0500

    fresh build

--- src/views/pages/misc/NotAuthorized.js
+++ src/views/pages/misc/NotAuthorized.js
@@ -29,63 +29,22 @@ const NotAuthorized = () => {
   return (
     <div className='misc-wrapper'>
       <Link className='brand-logo' to='/'>
-        <svg viewBox='0 0 139 95' version='1.1' height='28'>
+        <svg width="35" height="35" viewBox="0 0 57 57" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M54.698 14.3914L42.6086 2.30198C41.1348 0.82815 39.1336 0 37.0482 0H19.9518C17.8664 0 15.8672 0.82815 14.3914 2.30198L2.30198 14.3914C0.82815 15.8652 0 17.8664 0 19.9518V37.0482C0 39.1336 0.82815 41.1328 2.30198 42.6086L14.3914 54.698C15.8652 56.1719 17.8664 57 19.9518 57H37.0482C39.1336 57 41.1328 56.1719 42.6086 54.698L54.698 42.6086C56.1719 41.1348 57 39.1336 57 37.0482V19.9518C57 17.8664 56.1719 15.8672 54.698 14.3914ZM44.1687 39.4003C44.1687 42.0331 42.0351 44.1667 39.4023 44.1667H17.5997C14.9669 44.1667 12.8333 42.0331 12.8333 39.4003V17.5997C12.8333 14.9669 14.9669 12.8333 17.5997 12.8333H39.4003C42.0331 12.8333 44.1667 14.9669 44.1667 17.5997V39.4003H44.1687Z" fill="url(#paint0_linear_560_112960)" />
           <defs>
-            <linearGradient x1='100%' y1='10.5120544%' x2='50%' y2='89.4879456%' id='linearGradient-1'>
-              <stop stopColor='#000000' offset='0%'></stop>
-              <stop stopColor='#FFFFFF' offset='100%'></stop>
-            </linearGradient>
-            <linearGradient x1='64.0437835%' y1='46.3276743%' x2='37.373316%' y2='100%' id='linearGradient-2'>
-              <stop stopColor='#EEEEEE' stopOpacity='0' offset='0%'></stop>
-              <stop stopColor='#FFFFFF' offset='100%'></stop>
+            <linearGradient id="paint0_linear_560_112960" x1="15.5055" y1="8.6731" x2="45.0693" y2="53.7808" gradientUnits="userSpaceOnUse">
+              <stop stop-color="#54D86A" />
+              <stop offset="0.2114" stop-color="#177FFA" />
+              <stop offset="0.5987" stop-color="#5A5CD3" />
+              <stop offset="1" stop-color="#FE315A" />
             </linearGradient>
           </defs>
-          <g id='Page-1' stroke='none' strokeWidth='1' fill='none' fillRule='evenodd'>
-            <g id='Artboard' transform='translate(-400.000000, -178.000000)'>
-              <g id='Group' transform='translate(400.000000, 178.000000)'>
-                <path
-                  d='M-5.68434189e-14,2.84217094e-14 L39.1816085,2.84217094e-14 L69.3453773,32.2519224 L101.428699,2.84217094e-14 L138.784583,2.84217094e-14 L138.784199,29.8015838 C137.958931,37.3510206 135.784352,42.5567762 132.260463,45.4188507 C128.736573,48.2809251 112.33867,64.5239941 83.0667527,94.1480575 L56.2750821,94.1480575 L6.71554594,44.4188507 C2.46876683,39.9813776 0.345377275,35.1089553 0.345377275,29.8015838 C0.345377275,24.4942122 0.230251516,14.560351 -5.68434189e-14,2.84217094e-14 Z'
-                  id='Path'
-                  className='text-primary'
-                  style={{ fill: 'currentColor' }}
-                ></path>
-                <path
-                  d='M69.3453773,32.2519224 L101.428699,1.42108547e-14 L138.784583,1.42108547e-14 L138.784199,29.8015838 C137.958931,37.3510206 135.784352,42.5567762 132.260463,45.4188507 C128.736573,48.2809251 112.33867,64.5239941 83.0667527,94.1480575 L56.2750821,94.1480575 L32.8435758,70.5039241 L69.3453773,32.2519224 Z'
-                  id='Path'
-                  fill='url(#linearGradient-1)'
-                  opacity='0.2'
-                ></path>
-                <polygon
-                  id='Path-2'
-                  fill='#000000'
-                  opacity='0.049999997'
-                  points='69.3922914 32.4202615 32.8435758 70.5039241 54.0490008 16.1851325'
-                ></polygon>
-                <polygon
-                  id='Path-2'
-                  fill='#000000'
-                  opacity='0.099999994'
-                  points='69.3922914 32.4202615 32.8435758 70.5039241 58.3683556 20.7402338'
-                ></polygon>
-                <polygon
-                  id='Path-3'
-                  fill='url(#linearGradient-2)'
-                  opacity='0.099999994'
-                  points='101.428699 0 83.0667527 94.1480575 130.378721 47.0740288'
-                ></polygon>
-              </g>
-            </g>
-          </g>
         </svg>
-        <h2 className='brand-text text-primary ms-1'>Vuexy</h2>
+        <h2 className='brand-text text-primary ms-1'>Clatter AI</h2>
       </Link>
       <div className='misc-inner p-2 p-sm-3'>
         <div className='w-100 text-center'>
           <h2 className='mb-1'>You are not authorized! 🔐</h2>
-          <p className='mb-2'>
-            The Webtrends Marketing Lab website in IIS uses the default IUSR account credentials to access the web pages
-            it serves.
-          </p>
           <Button
             tag={Link}
             color='primary'
